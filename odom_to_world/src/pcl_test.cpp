18b4ca17be505383d87f6dcf3b1e1e2c8deb27b1
Jan Heuer <janheuer2211@gmail.com>
2013-10-10 23:02:52 +0200

    further pcl tests

--- odom_to_world/src/pcl_test.cpp
+++ odom_to_world/src/pcl_test.cpp
@@ -2,6 +2,7 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 
+
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
 int main(int argc, char** argv)
@@ -11,9 +12,11 @@ int main(int argc, char** argv)
   ros::Publisher pub = nh.advertise<PointCloud> ("points2", 1);
 
   PointCloud::Ptr msg (new PointCloud);
-  msg->header.frame_id = "world";
-  msg->height = msg->width = 1;
+  msg->header.frame_id = "pcl";
+  msg->height = 1;
+  msg->width = 2;
   msg->points.push_back (pcl::PointXYZ(1.0, 2.0, 3.0));
+  msg->points.push_back (pcl::PointXYZ(2.0, 1.0, 1.0));
 
   ros::Rate loop_rate(4);
   while (nh.ok())
@@ -24,3 +27,6 @@ int main(int argc, char** argv)
     loop_rate.sleep ();
   }
 }
+
+//http://gt-ros-pkg.googlecode.com/svn/trunk/hrl/hrl_behaviors/hrl_table_detect/src/surface_segmentation.cpp
+//http://ros-users.122217.n3.nabble.com/transformPointCloud-td2776476.html
