9e88b39b9c8c751b702dfa902a3dca537b2200db
m myself <jan.heuer@youpooly.de>
2013-10-18 17:32:12 +0200

    next try for pcl to tf

--- odom_to_world/src/pcl_test_to_tf.cpp
+++ odom_to_world/src/pcl_test_to_tf.cpp
@@ -3,32 +3,35 @@
 #include <pcl/point_types.h>
 #include <boost/foreach.hpp>
 
-#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+
+
 #include "pcl_ros/transforms.h"
 #include <boost/foreach.hpp>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
-ros::Publisher tf_pub;
 
-tf::TransformListener *tf_listener; 
+tf::TransformBroadcaster *broadcaster;
 
 
 void callback(const PointCloud::ConstPtr& pcl_in)
 {
 
-  PointCloud pcl_out;
-   
+  geometry_msgs::TransformStamped pcl_trans;
+  pcl_trans.header.frame_id = "world";
+  pcl_trans.child_frame_id = "pcl";
+    
+  // update transform
+  // (moving in a circle with radius=2)
+  pcl_trans.header.stamp = ros::Time::now();
+    
 
-  tf_listener->waitForTransform("/world", "/pcl", (*pcl_in).header.stamp, ros::Duration(5.0));
-  pcl_ros::transformPointCloud("/world", *pcl_in, pcl_out, *tf_listener);
-  //tf_pub.publish(pcl_out);
-
-
-
-  printf ("Cloud: width = %d, height = %d\n", pcl_in->width, pcl_in->height);
+  //send the transform
+  broadcaster->sendTransform(pcl_trans);
+  /*printf ("Cloud: width = %d, height = %d\n", pcl_in->width, pcl_in->height);
   BOOST_FOREACH (const pcl::PointXYZ& pt, pcl_in->points)
-    printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+    printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);*/
 }
 
 int main(int argc, char** argv)
@@ -36,13 +39,12 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "sub_pcl");
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe<PointCloud>("points2", 1, callback);
-  tf_pub = nh.advertise<PointCloud> ("tf_points2", 1);
+  broadcaster = new tf::TransformBroadcaster(); 
   
-  tf_listener    = new tf::TransformListener();
   
   ros::spin();
-  //delete tf_listener; 
-  //return 0; 
+  delete broadcaster;
+  return 0;
 }
 
 //https://robotics.wtb.tue.nl/svn/ros/code/tue-ros-pkg/dev/tue_executioner_dev/tue_open_cabinet_dev/src/handle_publisher.cpp
